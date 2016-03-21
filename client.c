a8095069a1b73df40104e4b166dc4dd2dbda88cb
Nicolas <nicolasLaurent9816@gmail.com>
2016-03-21 09:37:27 +0100

    Préparation pour fusion

--- client.c
+++ client.c
@@ -4,151 +4,264 @@
 #include<sys/socket.h>
 #include<netinet/in.h>
 #include<arpa/inet.h>
-#include<unistd.h>
+#include <unistd.h>
 #include<string.h>
 #include<strings.h>
 #include<netdb.h>
 #include"protocole.h"
+#include "file.h"
+#include <pthread.h>
+#include <fcntl.h>
+#include <errno.h>
 
+#ifndef EWOULDBLOCK
+	#define EWOULDBLOCK EAGAIN
+#endif
+
+
+
+static char pseudo[TAILLE_PSEUDO];
+ pthread_t th;
+
+fifo *recu;
+
+fifo* envois[50];		//à remplacer par une liste dynamique (à implémenter)
+int nbEnvoi=0;
 
 int saisir_texte(char *chaine, int longueur);
-void init_trame(trame *trame_init);
 
-int connexion(int sock,char *pseudo){
-  pid_t pid;
+
+void * connexion(void* envoy){	//Thread de connexion, 1 par connexion client-client
+  fifo* envoi=(fifo*)envoy;
+  int sock=envoi->sock;
+  printf("sock=%i\n",sock);
   trame trame_read;
   trame trame_write;
   trame trame1;
-//  char pseudo[TAILLE_PSEUDO];
+  char pseudo_dist[TAILLE_PSEUDO];
+  char datas[TAILLE_MAX_MESSAGE+32];
+  ssize_t result_read;
+  useconds_t timeToSleep=100;
 
-/*  printf("Choisir un pseudo: ");
-  saisir_texte(pseudo,TAILLE_PSEUDO);
-*/
-  init_trame(&trame1);
-  sprintf(trame1.pseudo_envoyeur,"%s",pseudo);
-  trame1.type_message.hello=1;
+  //Dis "bonjour !" en envoyant son pseudo
   bzero(trame1.message,TAILLE_MAX_MESSAGE);
+  strcpy(trame1.message,pseudo);
+  trame1.type_message=hello;
   trame1.taille=sizeof(trame1);
+  fcntl(sock,F_SETFL,fcntl(sock,F_GETFL)|O_NONBLOCK);
   write(sock,(void *)&trame1,trame1.taille);
 
-  pid=fork();
-   switch (pid){
-	case -1:
-		perror("Erreur fork");
-		exit(EXIT_FAILURE);
-		break;
-	case 0:
-		while(1){
-		  bzero(trame_read.message,TAILLE_MAX_MESSAGE);
-		  init_trame(&trame_read);
-		  if (read(sock,(void *)&trame_read,sizeof(trame_read))>0){
-		    if (trame_read.type_message.hello==1){
-			printf("%s vient de se connecter \n",trame_read.pseudo_envoyeur);
-		    }
-		    printf("[%s] %s\n",trame_read.pseudo_envoyeur,trame_read.message);
-		  } else return 2;
-		}
-		break;
-	default:
-		while (1){
-		  bzero(trame_write.message,TAILLE_MAX_MESSAGE);
-		  bzero(trame_write.pseudo_envoyeur,TAILLE_MAX_MESSAGE);
-		  init_trame(&trame_write);
-		  saisir_texte(trame_write.message,TAILLE_MAX_MESSAGE);
-		  trame_write.type_message.texte=1;
-		  sprintf(trame_write.pseudo_envoyeur,"%s",pseudo);
-		  trame_write.taille=sizeof(trame_write);
-		  write(sock,(void *)&trame_write,trame_write.taille);
-		}
-   } 
 
+
+
+  while(1){
+    bzero(trame_read.message,TAILLE_MAX_MESSAGE);
+    timeToSleep=1000;
+
+    //Phase lecture
+    errno=0;
+    if(0==read(sock,(void *)&trame_read,sizeof(trame_read))){
+      printf("Connexion interrompue\n");
+      close(sock);
+      return;
+    }
+    result_read=errno;
+
+
+    if ((result_read != EWOULDBLOCK)&&(result_read != EAGAIN)){
+      bzero(datas,sizeof(datas));
+      timeToSleep=1;
+      if (trame_read.type_message==hello){
+	strcpy(pseudo_dist,trame_read.message);
+	/*s*/printf(/*datas,*/"%s vient de se connecter \n",pseudo_dist);	//Parties en commentaire à laisser pour une utilisation future
+
+      }else if(trame_read.type_message==quit){
+	printf("Fermeture de connexion (en toute tranquillité)\n");
+	write(sock,(void *)&trame_read,trame_read.taille);
+	close(sock);
+	return;
+      }else
+	/*s*/printf(/*datas,*/"[%s] %s\n",pseudo_dist,trame_read.message);	//Parties en commentaire à laisser pour une utilisation future
+
+      //enfiler_fifo(recu, datas);						//Parties en commentaire à laisser pour une utilisation future
+    }
+
+    //Phase écriture
+    if(!(estVide_fifo(envoi))){
+
+      bzero(trame_write.message,TAILLE_MAX_MESSAGE);
+      timeToSleep=1;
+      defiler_fifo(envoi,trame_write.message);
+
+      if(0==strcmp("QUIT",trame_write.message))
+	trame_write.type_message=quit;
+      else
+	trame_write.type_message=texte;
+      trame_write.taille=sizeof(trame_write);
+      write(sock,(void *)&trame_write,trame_write.taille);
+    }
+
+    usleep(timeToSleep);
+
+  }
 }
 
 
-int main(int argc, char ** argv){
+
+void connectTO(char *adresse, int port){	//Etablit une connexion vers un client attendant
 
  int sock;
  struct sockaddr_in extremite_locale, extremite_distante;
  socklen_t length = sizeof(struct sockaddr_in);
  struct hostent *hote_distant;
- char pseudo[20];
- 
- printf("Choisir un pseudo: ");
- saisir_texte(pseudo,TAILLE_PSEUDO);
+ fifo *envoy;
 
  sock=socket(AF_INET, SOCK_STREAM, 0);
+
  if (sock==-1){
    perror ("Erreur appel sock ");
-   return EXIT_FAILURE;
+   exit(1);
  }
 
  extremite_locale.sin_family=AF_INET;
- extremite_locale.sin_addr.s_addr=inet_addr("127.0.0.1");
- extremite_locale.sin_port=0; 
+ extremite_locale.sin_addr.s_addr=htonl(INADDR_ANY);
+ extremite_locale.sin_port=0;
 
  if (bind(sock, (struct sockaddr *) &extremite_locale, sizeof(extremite_locale))==-1){
    perror("Erreur appel bind ");
-   return EXIT_FAILURE;
+   exit(1);
  }
 
- if (getsockname(sock,(struct sockaddr *) &extremite_locale, &length)<0){
-   perror("Erreur appel getsockname ");
-   return EXIT_FAILURE;
- }
 
- printf("\nOuverture d'un socket (n°%i) sur le port %i on mode connecté\n", sock, ntohs(extremite_locale.sin_port));
- printf("extremite locale :\n sin_family = %d\n sin_addr.s_addr = %s\n sin_port = %d\n\n", extremite_locale.sin_family, inet_ntoa(extremite_locale.sin_addr), ntohs(extremite_locale.sin_port));
-
- if (argc==3){
-   
-   if ((hote_distant=gethostbyname(argv[1]))==(struct hostent *)NULL){
-	fprintf(stderr, "%s:unknown host: %s\n", argv[0], argv[1]);
+   if ((hote_distant=gethostbyname(adresse))==(struct hostent *)NULL){
+	fprintf(stderr, "chat:unknown host: %s\n", *adresse);
 	exit(EXIT_FAILURE);
    }
 
    bzero((char *)&extremite_distante, sizeof(extremite_distante));
-   extremite_distante.sin_family=AF_INET;  
+   extremite_distante.sin_family=AF_INET;
    (void) bcopy ((char *)hote_distant->h_addr, (char *) &extremite_distante.sin_addr,hote_distant->h_length);
 
-   extremite_distante.sin_port=htons(atoi(argv[2]));
+   extremite_distante.sin_port=htons(port);
 
    if (connect(sock, (struct sockaddr *)&extremite_distante,sizeof(extremite_distante))==0){// On essaye de se connecter à un client distant
-     if (connexion(sock,pseudo)==2){
-     }
+      envoy=creer_fifo();
+      envoy->sock=sock;
+      envois[nbEnvoi]=envoy;
+      nbEnvoi++;
+    pthread_create(&th,NULL,connexion,(void*) envoy);
+
     }
    else printf("Echec de connexion a %s %d\n", inet_ntoa(extremite_distante.sin_addr),ntohs(extremite_distante.sin_port));
+
+//sleep(1); à supprimer car cette implémentation n'efface plus le port en fin de fonction
+
+}
+
+void * waitConnectFROM(){	//Attends d'autres clients pour connexion
+
+ int sock;
+ struct sockaddr_in extremite_locale, extremite_distante;
+ socklen_t length = sizeof(struct sockaddr_in);
+ struct hostent *hote_distant;
+ fifo *envoy;
+
+
+ sock=socket(AF_INET, SOCK_STREAM, 0);
+ if (sock==-1){
+   perror ("Erreur appel sock ");
+   exit(1);
  }
 
- printf("En attente de connexion.........\n"); 
+ extremite_locale.sin_family=AF_INET;
+ extremite_locale.sin_addr.s_addr=htonl(INADDR_ANY);
+ extremite_locale.sin_port=0;
+
+ if (bind(sock, (struct sockaddr *) &extremite_locale, sizeof(extremite_locale))==-1){
+   perror("Erreur appel bind ");
+   exit(1);
+ }
+
+ if (getsockname(sock,(struct sockaddr *) &extremite_locale, &length)<0){
+   perror("Erreur appel getsockname ");
+   exit(1);
+ }
+
+ printf("\nOuverture d'une socket (n°%i) sur le port %i on mode connecté\n", sock, ntohs(extremite_locale.sin_port));
+ printf("extremite locale :\n sin_family = %d\n sin_addr.s_addr = %s\n sin_port = %d\n\n", extremite_locale.sin_family, inet_ntoa(extremite_locale.sin_addr), ntohs(extremite_locale.sin_port));
+
+  printf("En attente de connexion.........\n");
 
  if (listen(sock, 2)<0){
      perror("Erreur appel listen ");
-     return EXIT_FAILURE;
+     exit(1);
  }
 
  while(1){
    int ear=accept(sock, (struct sockaddr *) &extremite_distante, &length);
    if (ear == -1){
      perror("Erreur appel accept ");
-     return EXIT_FAILURE;
+     exit(1);
    }
    printf("\nConnection établie\n\n");
-   
+
    printf("Connection sur la socket ayant le fd %d\nextremite distante\n sin_family : %d\n sin_addr.s_addr = %s\n sin_port = %d\n\n", ear, extremite_distante.sin_family, inet_ntoa(extremite_distante.sin_addr), ntohs(extremite_distante.sin_port));
-   
-   if (connexion(ear,pseudo)==2){
-   }
+
+    envoy=creer_fifo();
+    envoy->sock=ear;
+    envois[nbEnvoi]=envoy;
+    nbEnvoi++;
+
+   pthread_create(&th,NULL,connexion,(void*) envoy);
+
 
    }
+
+}
+
+
+
+int main(int argc, char ** argv){
+
+ recu=creer_fifo();
+ char datas[TAILLE_MAX_MESSAGE];
+ int agc;
+ int i;
+
+
+ printf("Choisir un pseudo: ");
+ saisir_texte(pseudo,TAILLE_PSEUDO);
+
+
+   for(agc=2;agc<argc;agc+=2)
+      connectTO(argv[(agc-1)], atoi(argv[agc]));
+
+
+ pthread_create(&th,NULL,waitConnectFROM,NULL);
+
+ while(1){
+
+   saisir_texte(datas,sizeof(datas));
+
+   for(i=0;i<nbEnvoi;i++)
+      enfiler_fifo(envois[i], datas);
+}
+
+
+
  return EXIT_SUCCESS;
 }
 
-int saisir_texte(char *chaine, int longueur){
+
+
+
+
+int saisir_texte(char *chaine, int longueur){	//un fgets perso
 
   char *entre=NULL;
 
   if (fgets(chaine,longueur,stdin)!=NULL){
- 
+
     entre=strchr(chaine,'\n');
     if (entre!=NULL){
 	*entre='\0';
@@ -158,20 +271,6 @@ int saisir_texte(char *chaine, int longueur){
   else return 0;
 }
 
-void init_trame(trame *trame_init){
 
-  trame_init->taille=0;
-  trame_init->type_message.hello=0;
-  trame_init->type_message.quit=0;
-  trame_init->type_message.texte=0;
-  trame_init->type_message.audio.proposition=0;
-  trame_init->type_message.audio.acceptation=0;
-  trame_init->type_message.audio.transfert=0;
-  trame_init->type_message.video.proposition=0;
-  trame_init->type_message.video.acceptation=0;
-  trame_init->type_message.video.transfert=0;
-  trame_init->type_message.image.proposition=0;
-  trame_init->type_message.image.acceptation=0;
-  trame_init->type_message.image.transfert=0;
 
-}
+
